1400490bdbc2a95b0949643b9f0613393d17c8d3
hashrock <hashedrock@gmail.com>
2015-11-16 18:42:19 +0900

    Add cli

--- server.js
+++ server.js
@@ -3,32 +3,60 @@ var path = require('path');
 var app = loopback();
 var fs = require("fs");
 var bodyParser = require('body-parser');
+var open = require("open");
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(loopback.static(path.resolve(__dirname, './client')));
 
-app.get('/json', function(req, res){
-  fs.readFile('data.json', 'utf8', function(err, data){
+var run = function(filename){
+  fs.access(filename, fs.R_OK | fs.W_OK, function (err) {
     if(err){
-      res.send({
-        status: "failed",
-        message: err
-      })
-      console.log(err);
-    }else{
-      res.send({
-        status: "success",
-        data: data
-      });
+      if(err.code === "ENOENT"){
+        console.log("Create : " + filename);
+        var sample = [{"Title":"Example","Contents":"hoo bar"},{"Title":"Example2","Contents":"hoge huga"},{"Title":"test","Contents":"123"}];
+        fs.writeFileSync(filename, JSON.stringify(sample), "utf-8");
+      }else{
+        console.log(err.code);
+        return;
+      }
     }
+    app.get('/json', function(req, res){
+      fs.readFile(filename, 'utf8', function(err, data){
+        if(err){
+          res.send({
+            status: "failed",
+            message: err
+          })
+          console.log(err);
+        }else{
+          res.send({
+            status: "success",
+            data: data
+          });
+        }
+      });
+    });
+    
+    app.post("/json", function(req, res){
+      fs.writeFileSync(filename, req.body.data, "utf-8");
+      res.send({
+        status: "success"
+      })
+    })
+    
+    console.log("http://localhost:8989/");
+    app.listen(8989);
+    open("http://localhost:8989/");
+    
+    
   });
-});
+  
+}
 
-app.post("/json", function(req, res){
-  fs.writeFileSync("data.json", req.body.data, "utf-8");
-  res.send({
-    status: "success"
-  })
-})
 
-app.listen(3000);
+module.exports = {
+  run: run
+}
+
+
